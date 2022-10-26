cfdd7fcbec5f8b45e9382930a45cac0b3343185b
Miad Hasan <mdhasan9.sy@gmail.com>
2022-10-26 14:54:22 +0600

    Some funtionality in login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; 
+import { LoginDefaultComponent } from './login/login-default/login-default.component';
+import {HomeDefaultComponent} from './home/home-default/home-default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginDefaultComponent },
+  { path: 'home', component: HomeDefaultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
